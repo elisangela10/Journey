094bb16fdcd76084fda266c4b4ea1df8113241e6
Elisangela Miranda dos Anjos <50150279+elisangela10@users.noreply.github.com>
2024-07-15 00:22:56 -0300

    Template and RegisterValidate

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,22 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            var useCase = new ResgisterTripUseCase();
+            
+            useCase.Execute(request);
+
+            return Created(); 
+
+        }
+    }
+}
